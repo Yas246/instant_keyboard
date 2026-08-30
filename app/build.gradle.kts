5305b7a0a192ebafd98d32e8f833b1f68152a789
Yas246 <bigwabi01@gmail.com>
2026-08-30 18:45:43 +0100

    chore: versionCode 3907

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "helium314.keyboard"
         minSdk = 21
         targetSdk = 35
-        versionCode = 3906
+        versionCode = 3907
         versionName = "3.9-sons"
         ndk {
             abiFilters.clear()
